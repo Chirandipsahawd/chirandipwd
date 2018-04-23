ccbe622ea68911084777fe6aaeee1dc11012ad47
Chirandipsahawd <33371991+Chirandipsahawd@users.noreply.github.com>
2018-04-23 11:18:33 +0530

    Create function.php
    How to remove the WooCommerce results count
    How to remove default sorting dropdown
    How to add woocommerce  theme support

---
+++ function.php
@@ -0,0 +1,15 @@
+// woocommerce  theme support
+
+add_action( 'after_setup_theme', 'setup_woocommerce_support' );
+
+ function setup_woocommerce_support()
+{
+  add_theme_support('woocommerce');
+}
+
+// remove default sorting dropdown
+ 
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+
+// Remove the result count from WooCommerce
+remove_action( 'woocommerce_before_shop_loop' , 'woocommerce_result_count', 20 );

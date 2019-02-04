1dab24facf750c1af913cdc709c4edd9ab661457
Chirandipsahawd <33371991+Chirandipsahawd@users.noreply.github.com>
2019-02-04 13:54:36 +0530

    Update function.php

--- function.php
+++ function.php
@@ -331,3 +331,26 @@ add_filter('loop_shop_per_page','dl_sort_by_page');
 add_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_page_ordering', 20 );
 
 /*=================================================procduct dropdown counter end===============================================*/
+
+
+
+/**
+ * Add the product's short description (excerpt) to the WooCommerce shop/category pages. The description displays after the product's name, but before the product's price.
+ *
+ * Ref: https://gist.github.com/om4james/9883140
+ *
+ * Put this snippet into a child theme's functions.php file
+ */
+function woocommerce_after_shop_loop_item_title_short_description() {
+	global $product;
+	if ( ! $product->post->post_excerpt ) return;
+	?>
+	<div itemprop="description">
+		<?php echo apply_filters( 'woocommerce_short_description', $product->post->post_excerpt ) ?>
+	</div>
+	<?php
+}
+add_action('woocommerce_after_shop_loop_item_title', 'woocommerce_after_shop_loop_item_title_short_description', 5);
+
+
+//I had to use echo $product->get_short_description() instead:
